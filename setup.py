d18c45da7416e11c7b7553db95da19487686ade4
Jordan Reiter <jordan@aace.org>
2018-01-31 14:23:43 -0500

    Bumped version and added Django requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='django-flexibledatefield',
-      version='1.1',
+      version='2.0',
       description='A custom field that can store a date with flexible granularity (i.e. only year, year+month, or full date)',
       author='Jordan Reiter',
       author_email='jordanreiter@gmail.com',
       packages=['flexibledatefield'],
-     )
+      install_requires=['Django>=1.11'],
+)
