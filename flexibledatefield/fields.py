2ef00f277be97f7cc442281a24fcc177c645d4d6
c r <3wc.git@doesthisthing.work>
2020-07-12 20:55:57 +0200

    Update from_db_value for new Django API

--- flexibledatefield/fields.py
+++ flexibledatefield/fields.py
@@ -150,7 +150,7 @@ class FlexibleDateDescriptor(object):
 
 class FlexibleDateField(models.PositiveIntegerField):
 
-    def from_db_value(self, value, expression, connection, context):
+    def from_db_value(self, value, expression, connection):
         if value is None:
             return None
         return flexibledate.parse(value)
